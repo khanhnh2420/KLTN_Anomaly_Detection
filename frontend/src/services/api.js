08ceb82b0c83d8e1252d7e2fbd353c5fff809216
KhanhNH2420 <hoangkhanh2420@gmail.com>
2025-12-25 00:28:34 +0700

    Update scikit-learn==1.6.1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,25 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
 
-export const scoreCsv = async (file, page, pageSize) => {
-  const form = new FormData();
-  form.append("file", file);
+const api = axios.create({
+baseURL: import.meta.env.VITE_API_URL,
+});
 
-  const res = await axios.post(
-    `${API}/score_csv?page=${page}&page_size=${pageSize}`,
-    form
-  );
 
-  return res.data;
+export const scoreCSV = async (file, page = 1, pageSize = 20) => {
+const formData = new FormData();
+formData.append("file", file);
+
+
+const res = await api.post(
+`/score_csv?page=${page}&page_size=${pageSize}`,
+formData,
+{ headers: { "Content-Type": "multipart/form-data" } }
+);
+
+
+return res.data;
 };
+
+
+export default api;
