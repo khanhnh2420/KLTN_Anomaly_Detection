1bd79fd1775ec6fe647c9200cc8e91c02b27aecc
KhanhNH2420 <hoangkhanh2420@gmail.com>
2025-12-26 03:41:34 +0700

    BEBE

--- backend/src/main.py
+++ backend/src/main.py
@@ -55,14 +55,13 @@ app = FastAPI(
 )
 
 # =========================
-# CORS (BẮT BUỘC CHO FE)
+# CORS
 # =========================
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://sap-anomaly-fe.onrender.com",
-    ],  # khi production có thể giới hạn domain
+        "https://sapanomalydetect.netlify.app",
+    ],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
